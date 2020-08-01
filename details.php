1c3e453b78f911cf20250457dc6e610517070489
paxtonhyde <paxify@gmail.com>
2020-08-01 17:40:05 -0600

    minor change

--- details.php
+++ details.php
@@ -9,23 +9,34 @@ if(!isset($_SESSION['household_id'])) {
 
 $house=new HouseData($msi,$smarty,$_SESSION['household_id']);
 
-if(isset($_GET['action']) && $_GET['action']=='save') {
-  $house->updateHouse($msi, $smarty);
-}
-
-$ErrMsg='';
-if(isset($_POST['buttonAction'])) {
-  if($_POST['buttonAction']=='house') {
-    $ErrMsg=buildErrorMessage($ErrMsg,'selected a household');
-    // Do the SQL
-
-  } else if($_POST['buttonAction']=='contact') {
-    $ErrMsg=buildErrorMessage($ErrMsg,'selected a member');
-    
+if(isset($_GET['action'])){
+  if($_GET['action']=='save') {
+    $house->updateHouse($msi, $smarty);
   } else {
-    $ErrMsg=buildErrorMessage($ErrMsg,'buttonAction: '.$_POST['buttonAction']);
-  }
+    $ErrMsg='';
+    if(isset($_POST['buttonAction'])) {
+      /* buttonAction (search param) */
+      if($_POST['buttonAction']=='house') {
+        $ErrMsg=buildErrorMessage($ErrMsg,'selected a household');
 
+      } else if($_POST['buttonAction']=='contact') {
+        $ErrMsg=buildErrorMessage($ErrMsg,'selected a member');
+    
+      } else {
+        $ErrMsg=buildErrorMessage($ErrMsg,'buttonAction: '.$_POST['buttonAction']);
+      }
+    
+      /* action (add or move form) */
+      if ($_GET['action']=='move'){
+
+      } else if ($_GET['action']=='add') {
+
+      } else {
+        // pass
+      }
+  
+    }
+  }
 }
 displayFooter($smarty,$ErrMsg);
 
