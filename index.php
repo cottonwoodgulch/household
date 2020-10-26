8e1fb9ac41a86ed58a6bd125a46ab53d9ff9f6b6
Thomas Hyde <tom@russellhyde.net>
2020-10-26 10:58:18 -0400

    fix start from gulchdbi

--- index.php
+++ index.php
@@ -4,6 +4,13 @@
    if user is not logged in, libe.php redirects to login.php */
 
 require_once 'libe.php';
-header("Location: summary.php");
+
+if(isset($_GET['cid'])) {
+  /* Arrived here from gulchdbi. I think it's ok to retrieve 
+     the household even though the user may not be logged in  */
+  $_SESSION['household_id']=getHouseholdFromContact($msi,$smarty,$_GET['cid']);
+}
+
+header("Location: details.php");
 
 ?>
